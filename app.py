82aa68a3530fb3ca5fee47ecd9f9ee8ccd9ae205
waghrayrashi <61018806+waghrayrashi@users.noreply.github.com>
2020-08-11 21:33:24 -0400

    app code

--- app.py
+++ app.py
@@ -18,6 +18,25 @@ from flask import Flask, jsonify, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 ##########################################
+# Database Connection Set up
+##########################################
+# Setup SQLAlchemy with flask
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', '')
+# # Create database and pass in the app 
+# db = SQLAlchemy(app)
+
+# # Create engine
+# conn_str = "postgres://rilcwxqbdcttnx:6fbe02be55d33cf132b076fcec9f961162a0eb3ffc67aff912d8aedeabfcfe5e@ec2-34-237-89-96.compute-1.amazonaws.com:5432/d3c3p19kp6bchj"
+# engine = create_engine(conn_str)
+
+# # Reflect the database 
+# Base = automap_base()
+
+# Reflect the tables into SQLAlchemy classes
+# Base.prepare(engine, reflect=True)
+# allclasses = Base.classes.keys()
+
+##########################################
 # Flask Set up
 ##########################################
 app = Flask(__name__)
@@ -34,12 +53,54 @@ def index():
     return render_template("index.html")
 
 #######################################################
+# Render the database table 1 BDR
+#######################################################
+# @app.route("/nc1bdr")
+# def nc1bdr():
+#     """Query the database for 1 BDR houses"""
+
+#     # Save references to each table in database 
+#     Propertytypes = Base.classes.propertytypes
+
+#     # Create session
+#     session = Session(bind=engine)
+    
+#     # Query the table for columns of interest
+#     propertytypes = session.query(Propertytypes).all()
+    
+#     # Create an empty list
+#     property_types = []
+    
+#     # Loop through each row and create dictionaries for each column   
+#     for listing in propertytypes:
+#         ptypes_dict = {
+#             "propertytype": listing.propertytype,
+#             "listings": listing.listings
+#             # "percent": listing.percent
+#             }
+        
+#         # Append all columns to the object
+#         property_types.append(ptypes_dict)
+    
+#     session.close()
+#     # Return the json
+#     return jsonify(property_types)
+
+#######################################################
 # Render the Dashboard HTML 
 #######################################################
 @app.route("/dashboard")
 def dashboard():
     """Return the Dashboard page."""
     return render_template("rw_dashboard.html")
+
+#######################################################
+# Render the Conclusions HTML 
+#######################################################
+@app.route("/conclusions")
+def conclusions():
+    """Return the Conclusions page."""
+    return render_template("rw_conclusions.html")
 
 #######################################################
 # Render the Analysis Methodology HTML
@@ -62,35 +123,40 @@ def acknowledgements():
 #######################################################
 @app.route("/analysis")
 def analysis():
-    """Return the analysis page."""
+    """Return the exploratory analysis page."""
     return render_template("rw_analysis.html")
 
 ########################################################
-# Render the change over time plot HTML 
+# Render the 1 BDR price prediction HTML 
 #######################################################
-@app.route("/growth")
-def growth():
-    """Return the growth page."""
-    return render_template("rw_growth.html")
+@app.route("/prediction1bdr")
+def prediction1bdr():
+    """Return the 1 BDR price prediction page."""
+    return render_template("rw_prediction1bdr.html")
 
 ########################################################
-# Render the pricemap HTML 
+# Render the 2 BDR price prediction HTML 
 #######################################################
-@app.route("/pricemap")
-def pricemap():
-    """Return the pricemap page."""
-    return render_template("rw_pricemap.html")
+@app.route("/prediction2bdr")
+def prediction2bdr():
+    """Return the 2 BDR price prediction page."""
+    return render_template("rw_prediction2bdr.html")
 
 ########################################################
-# Render the prediction HTML 
+# Render the 3 BDR price prediction HTML 
 #######################################################
-@app.route("/prediction")
-def prediction():
-    """Return the prediction page."""
-    return render_template("rw_prediction.html")
-#############################################################################################
-# Rashi's additional routes
-############################################################################################
+@app.route("/prediction3bdr")
+def prediction3bdr():
+    """Return the 3 BDR price prediction page."""
+    return render_template("rw_prediction3bdr.html")
+
+########################################################
+# Render the 4 BDR price prediction HTML 
+#######################################################
+@app.route("/prediction4bdr")
+def prediction4bdr():
+    """Return the 4 BDR price prediction page."""
+    return render_template("rw_prediction4bdr.html")
 
 ###################################################################
 # Code to actually run the app
